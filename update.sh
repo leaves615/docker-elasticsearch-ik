fde048f05dab55c3ce23995f70fe4d243f72fe7c
leaves chen <leaves615@gmail.com>
2024-06-27 10:03:48 +0800

    generate dockerfile in action

--- update.sh
+++ update.sh
@@ -2,16 +2,18 @@
 ### https://blog.dockbit.com/templating-your-dockerfile-like-a-boss-2a84a67d28e9
 render() {
 sedStr="
-  s/%%VERSION%%/$version/g;
+  s/%%VERSION%%/$1/g;
 "
 
 sed -e "$sedStr" $1
 }
 
-versions=(7.10.1 7.11.2 7.12.1 7.13.4 7.14.2 7.16.3 7.17.6 8.1.3 8.2.3 8.3.3 8.4.3)
-for version in ${versions[*]}; do
-  if [ ! -d $version ]; then
-    mkdir $version
-  fi
-  render Dockerfile.template > $version/Dockerfile
-done
+render Dockerfile.template > $1/Dockerfile
+
+# versions=(7.10.1 7.11.2 7.12.1 7.13.4 7.14.2 7.16.3 7.17.6 8.1.3 8.2.3 8.3.3 8.4.3)
+# for version in ${versions[*]}; do
+#   if [ ! -d $version ]; then
+#     mkdir $version
+#   fi
+#   render Dockerfile.template > $version/Dockerfile
+# done
